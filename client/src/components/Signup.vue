44eca99cc463e0c38201bf5494402d154cde355c
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-08-14 01:36:09 +0900

    add signin, signup

---
+++ client/src/components/Signup.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-app style="margin-top: 50px;">
+        <div style="text-align: center;">
+            <br>
+            <img width="200px" src="../assets/logo.png" />
+            <br>
+            <br>
+        </div>
+        <v-card width="400px" class="mx-auto mt-5  blue-grey--text text--darken-2">
+            <v-card-title class="justify-center">
+                <h1 class="display-1">新規登録</h1>
+            </v-card-title>
+            <v-divider />
+            <v-card-text>
+                <v-form>
+                    <v-text-field prepend-icon="mdi-account" label="User ID" v-model="user_id" />
+                    <v-text-field prepend-icon="mdi-email" label="Email" v-model="email" />
+                    <v-text-field v-bind:type="showPassword ? 'text' : 'password'" prepend-icon="mdi-lock"
+                        v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" label="Password"
+                        @click:append="showPassword = !showPassword" v-model="password" />
+                    <v-text-field v-bind:type="showPassword ? 'text' : 'password'" prepend-icon="mdi-lock"
+                        v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" label="Confirm Password"
+                        @click:append="showPassword = !showPassword" v-model="password_confirm" />
+                    <v-card-actions class="justify-center">
+                        <v-btn class="info">登録</v-btn>
+                    </v-card-actions>
+                </v-form>
+            </v-card-text>
+            <v-divider />
+            <br>
+            <p class="text-center">すでにアカウントを<br>お持ちの方は<router-link to="/signin">こちら</router-link>
+            </p>
+        </v-card>
+    </v-app>
+</template>
+<script>
+export default {
+    name: 'Signup',
+    data: function () {
+        return {
+            showPassword: false,
+            user_id: '',
+            email: '',
+            password: '',
+            password_confirm: ''
+        }
+    }
+}
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,29 +1,31 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Signup from '@/components/Signup'
+import Signin from '@/components/Signin'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+export default new Router({
+    routes: [
+        {
+            path: '*',
+            redirect: 'signin'
+        },
+        {
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: '/signup',
+            name: 'Signup',
+            component: Signup
+        },
+        {
+            path: '/signin',
+            name: 'Signin',
+            component: Signin
+        }
+    ]
 })
-
-export default router
