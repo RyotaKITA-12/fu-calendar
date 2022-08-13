6ed13fc79f7f092b1e2eeeb7b9c0b550ad0e8567
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-08-13 21:33:41 +0900

    init client

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,32 @@
 <template>
-    <v-app>
-        <v-main>
-            <Calendar />
-        </v-main>
-    </v-app>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import Calendar from './components/Calendar';
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-    name: 'App',
+nav {
+  padding: 30px;
+}
 
-    components: {
-        Calendar,
-    },
-};
-</script>
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
