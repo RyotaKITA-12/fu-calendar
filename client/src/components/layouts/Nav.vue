3e19f9803c6ca4dda623ca704049ae1ab88ff2a8
RyotaKITA-12 <s2022007@stu.musashino-u.ac.jp>
2022-08-18 22:27:52 +0900

    init & add calendar

---
+++ client/src/components/layouts/Nav.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app-bar app color="white" flat>
+        <v-container class="py-0 fill-height">
+            <v-img max-height="50" max-width="200" src="@/assets/logo.png"></v-img>
+
+            <v-spacer></v-spacer>
+            <v-btn v-for="[icon, text] in links" :key="icon" text>
+                <v-icon>{{ icon }}</v-icon>
+                {{ text }}
+            </v-btn>
+
+            <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
+        </v-container>
+    </v-app-bar>
+</template>
+<script>
+export default {
+    name: 'App',
+    data: () => ({
+        links: [
+            ['mdi-plus', 'Add'],
+            ['mdi-magnify', 'Search'],
+            ['mdi-bell', 'Notice'],
+            ['mdi-logout', 'Logout'],
+        ],
+    }),
+}
+</script>
+
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,51 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import SignUp from '../views/SignUp.vue'
+import SignIn from '../views/SignIn.vue'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
+const routes = [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+        path: '/',
+        name: 'home',
+        component: Home,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/signin',
+        name: 'SignIn',
+        component: SignIn
+    },
+    {
+        path: '/signup',
+        name: 'SignUp',
+        component: SignUp
+    },
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
+
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+    if (requiresAuth) {
+        const user = sessionStorage.getItem('user')
+        if (!user) {
+            next({
+                path: '/signin',
+                query: { redirect: to.fullPath }
+            })
+        } else {
+            next()
+        }
+    } else {
+        next()
+    }
+})
+
+export default router
